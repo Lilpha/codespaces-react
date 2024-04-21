172fa606e45b1b54aeb1b7ab9a7aa754b2483f22
Lilpha <sungho2558@naver.com>
2024-04-21 11:13:38 +0000

    something is comeing

--- src/Copy/KakaoMap.jsx
+++ src/Copy/KakaoMap.jsx
@@ -1,6 +1,12 @@
 import { Map } from "react-kakao-maps-sdk"
 import useKakaoLoader from "./useKakaoLoader"
 
+const styles = {
+  inlineElem :{display:'inline-block'},
+  width: "50vw",
+  height: "90vh",
+}
+
 export default function BasicMap() {
   useKakaoLoader()
 
@@ -12,11 +18,7 @@ export default function BasicMap() {
         lat: 33.450701,
         lng: 126.570667,
       }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "350px",
-      }}
+      style={{height :styles.height,width: styles.width}}
       level={3} // 지도의 확대 레벨
     />
   )
--- src/Copy/MainContainer.jsx
+++ src/Copy/MainContainer.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import NavBar from './NavBar';
 import ImageList from './ImageList'
 
+const styles = {
+    inlineElem :{display:'inline-block'}
+}
+
 function MainContainer(props){
     return(
-        <div>
+        <div style={{width:'50vw'}}>
         <NavBar/>
         <ImageList/>
         </div>
