d50a611430258a78fab67e5d04189bdd73cac772
KimtheCook <151853923+KimtheCook@users.noreply.github.com>
2024-04-26 14:59:58 +0000

    Fkin center

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,13 @@ import ImageList from './Copy/ImageList'
 import MainContainer from './Copy/MainContainer';
 import KakaoMap from './Copy/KakaoMap'
 import NavBar from './Copy/NavBar';
+import Banner from './Copy/Banner';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Banner/>
     <NavBar/>
       <div style={{display:'flex'}}>
         <MainContainer/>
