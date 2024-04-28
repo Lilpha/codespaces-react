742f701ded5ff8f03763aaa9b14cc8d0c2f73c15
KimtheCook <151853923+KimtheCook@users.noreply.github.com>
2024-04-28 14:00:32 +0000

    vacation!!

--- src/Copy/Banner.jsx
+++ src/Copy/Banner.jsx
@@ -12,12 +12,16 @@ const styles = {
     height: "25px",
   },
   options: {
+    padding: "5px",
     display: "flex",
     border: "1px solid grey",
+    borderRadius: "10px",
     alignItems: "center",
   },
-  flexItems: {display:'flex',
-justifyContent:'space-around'},
+  flexItems: { display: "flex", justifyContent: "space-around" },
+  divText:{
+    padding:'5px'
+  }
 };
 function Banner(props) {
   return (
@@ -32,15 +36,13 @@ function Banner(props) {
         />
       </div>
       <div style={styles.flexItems}>
-        <div style={styles.wrapper}>
-          <div style={styles.options}>홍대입구</div>
-          <div style={styles.options}>언제든 일주일</div>
-          <div style={styles.options}>게스트 추가</div>
+        <div style={styles.options}>
+          <div style={styles.divText}>홍대입구</div>|<div style={styles.divText}>언제든 일주일</div>|<div style={styles.divText}>게스트 추가</div>
         </div>
       </div>
-      
+
       <div style={styles.flexItems}>
-        <div>당신의 공간을 에어비앤비하세요.</div>
+        <div style={styles.divText}>당신의 공간을 에어비앤비하세요.</div>
         <img
           style={styles.image}
           src="https://png.pngtree.com/png-clipart/20230418/original/pngtree-globe-line-icon-png-image_9065393.png"
