ba710e03553f8ac89c513f3fc9e3038ad55c6ab3
KimtheCook <151853923+KimtheCook@users.noreply.github.com>
2024-05-19 14:56:21 +0000

    did something

--- src/Copy/KakaoMap.jsx
+++ src/Copy/KakaoMap.jsx
@@ -1,4 +1,5 @@
-import { Map } from "react-kakao-maps-sdk"
+import { Map, MapInfoWindow } from "react-kakao-maps-sdk"
+
 import useKakaoLoader from "./useKakaoLoader"
 
 const styles = {
@@ -7,19 +8,11 @@ const styles = {
   height: "90vh",
 }
 
+
+
+
 export default function BasicMap() {
   useKakaoLoader()
-
-var iwContent = '<d-iv style="padding:5px;">Hello World!</div>', 
-    iwPosition = new kakao.maps.LatLng(33.450701, 126.570667),
-    iwRemoveable = true;
-var infowindow = new kakao.maps.InfoWindow({
-    map: map,
-    position : iwPosition, 
-    content : iwContent,
-    removable : iwRemoveable
-});
-
   return (
     <Map // 지도를 표시할 Container
       id="map"
@@ -30,6 +23,19 @@ var infowindow = new kakao.maps.InfoWindow({
       }}
       style={{height :styles.height,width: styles.width}}
       level={3} // 지도의 확대 레벨
-    />
-  )
+    >
+
+    <MapInfoWindow // 인포윈도우를 생성하고 지도에 표시합니다
+        position={{
+          // 인포윈도우가 표시될 위치입니다
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        removable={true} // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+      >
+        {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
+        <div style={{ padding: "5px", color: "#000" }}>Hello World!</div>
+      </MapInfoWindow>
+  </Map>  
+)
 }
