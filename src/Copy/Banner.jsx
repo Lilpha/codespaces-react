f4dc76ba8c4aa7ae688783e1a828df71ba3f0fa5
Lilpha <sungho2558@naver.com>
2024-06-19 14:37:29 +0000

    useState

--- src/Copy/Banner.jsx
+++ src/Copy/Banner.jsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import SingUpModal from "./bootstrap/SingUpModal";
+import StaticModal from "./bootstrap/StaticModal";
 import { ButtonGroup } from "react-bootstrap";
 
 //https://velog.io/@moolbum/%EC%B2%B4%ED%81%AC%ED%95%9C%EA%B2%83-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EB%A0%8C%EB%8D%94%ED%95%98%EA%B8%B0
@@ -33,7 +34,15 @@ const styles = {
 
 
 function Banner(props) {
-  
+  const var1 = {
+    title : 'This is Title',
+    body : 'This is Body'
+  }
+const [content, setContent] = useState();
+
+function handleEvent(){
+  setContent(<StaticModal title={var1.title} body={var1.body}/>);
+}
 const [singUpModalOn, setSignModalOn] = useState(false);
   return (
     
@@ -50,9 +59,10 @@ const [singUpModalOn, setSignModalOn] = useState(false);
       </div>
       <div style={styles.flexItems}>
         <div style={styles.options}>
-      <Button variant="secondary">청량리역</Button>
+      <Button variant="secondary" onClick={handleEvent}>청량리역</Button>
       <Button variant="secondary">언제든</Button>
       <Button variant="secondary">1명</Button>
+      <div>{content}</div>
         </div>
       </div>
 
