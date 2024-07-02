136a9b42a0d530534037b4bdd9d4191cf2d41129
KimtheCook <151853923+KimtheCook@users.noreply.github.com>
2024-07-02 14:13:41 +0000

    autonized

---
+++ src/Copy/GetLatLng.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect} from "react"
+
+
+
+function GetLatLng(props) {
+    var pattern = /!3d([\d.]+)!4d([\d.]+)/;
+    var match = props.match(pattern);
+
+    if (match) {
+        var latitude = parseFloat(match[1]);
+        var longitude = parseFloat(match[2]);
+        console.log(longitude, latitude);
+    } else {
+        return console.log("Not Found");
+    }
+
+    function getLat(){
+
+    }
+
+    function getLng(){
+        
+    }
+    return(
+        null
+    )
+    
+}
+
+export default GetLatLng;
+
+/*URL 형식 분석: 링크를 보면 !3d와 !4d라는 키워드가 좌표를 나타내는 것을 알 수 있습니다.
+정규 표현식 사용: URL에서 !3d와 !4d 뒤에 따라오는 숫자와 소수점을 추출하기 위해 정규 표현식을 사용할 수 있습니다.
+문자열 처리: 추출된 문자열을 실수형으로 변환하여 좌표값을 얻을 수 있습니다.
+*/
--- src/Copy/ImageMarker.jsx
+++ src/Copy/ImageMarker.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Map, MapInfoWindow } from "react-kakao-maps-sdk"
 import { render } from "@testing-library/react";
+import {GetLatLng} from "./GetLatLng"
 
 var text = 'https://www.google.co.kr/maps/place/%EC%97%90%EC%9D%B4%EC%B9%98%EC%97%90%EB%B9%84%EB%89%B4%ED%98%B8%ED%85%94+%EC%9D%B4%EB%8C%80%EC%A0%90/data=!4m17!1m7!3m6!1s0x357c99bd7b06f5f9:0xa292cc959edb4664!2z7ISc7Jq47J2Y7KeR!8m2!3d37.5592806!4d126.939512!16s%2Fg%2F11vlhfthml!3m8!1s0x357c98974d3cb641:0xf79c14e3b13cd3e8!5m2!4m1!1i2!8m2!3d37.5561936!4d126.9425616!16s%2Fg%2F11fxb8k1jk?hl=ko&entry=ttu'
 var url = text.split('/',6);
@@ -8,23 +9,28 @@ console.log(decodeURIComponent(url[5]))
 
 export const list =[
     {
+        img:'https://a0.muscache.com/im/pictures/17af19bc-eb72-417c-8824-fbb44a06218b.jpg?im_w=720',
         url:'https://www.google.co.kr/maps/place/%EC%9E%90%EC%9E%91%EB%8F%84%ED%95%B4%EB%B3%80/data=!4m6!3m5!1s0x5fd894db4b106483:0xee28a83305d68928!8m2!3d38.3087232!4d128.5427113!16s%2Fg%2F119w1h5c_?hl=ko&entry=ttu',
-        lat:'35.450701',
-        lng:'126.570667',
+        lat: GetLatLng(url, lat),
+        lng: GetLatLng(url, lng),
         info : "test 1"
     },
-    {   url:'https://www.google.co.kr/maps/place/%EC%86%8D%EC%B4%88%EB%93%B1%EB%8C%80/data=!4m6!3m5!1s0x5fd8bc0e94bdde21:0x9713df8d33326159!8m2!3d38.2136895!4d128.5999722!16s%2Fg%2F1tdvk045?hl=ko&entry=ttu',
+    {   
+        img:'https://a0.muscache.com/im/pictures/hosting/Hosting-1128902010827640927/original/60b308eb-1d33-431d-9b2a-73a692152cc5.jpeg?im_w=720',
+        url:'https://www.google.co.kr/maps/place/%EC%86%8D%EC%B4%88%EB%93%B1%EB%8C%80/data=!4m6!3m5!1s0x5fd8bc0e94bdde21:0x9713df8d33326159!8m2!3d38.2136895!4d128.5999722!16s%2Fg%2F1tdvk045?hl=ko&entry=ttu',
         lat:'33.450701',
         lng:'126.570667',
         info : "test 2"
     },
     {
+        img:'https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MjI3MjQxMzM%3D/original/5498d161-c2fb-462c-8f3b-d7eae180fd8e.jpeg?im_w=720',
         url : 'https://www.google.co.kr/maps/place/%EC%A0%9C%EC%9E%84%EC%8A%A4%EB%B8%94%EB%A3%A8/data=!4m9!3m8!1s0x5fd8bc7470d7f9a9:0x766ad580a9144668!5m2!4m1!1i2!8m2!3d38.207675!4d128.5933513!16s%2Fg%2F1pp2vcqk1?hl=ko&entry=ttu',
         lat:'37.5418281',
         lng:'126.948357',
         info:'text3'
     },
     {
+        img:'https://a0.muscache.com/im/pictures/miso/Hosting-1118741114104832403/original/81bb8500-684c-4937-8463-42e7c782891c.jpeg?im_w=720',
         url:'https://www.google.co.kr/maps/place/%EC%9E%90%EC%9E%91%EB%8F%84%ED%95%B4%EB%B3%80/data=!4m6!3m5!1s0x5fd894db4b106483:0xee28a83305d68928!8m2!3d38.3087232!4d128.5427113!16s%2Fg%2F119w1h5c_?hl=ko&entry=ttu',
         lat:'35.450701',
         lng:'126.570667',
@@ -32,10 +38,6 @@ export const list =[
     }
 
 ];
-console.log(list[0].url.split('!',9)[5])
-console.log(list[0].url.split('!',9)[4])
-console.log(decodeURIComponent(list[0].url.split('/',6)[5]))
-
 
 function ImageMarker(props) {
     return (
